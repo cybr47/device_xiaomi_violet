728ce0cdfe218acb077527062fd058f580237016
Raman <81064836+raman047@users.noreply.github.com>
2021-10-21 08:08:25 +0530

    Update and rename stag_violet.mk to revengeos_violet.mk

---
+++ revengeos_violet.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from violet device
+$(call inherit-product, device/xiaomi/violet/device.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/revengeos/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+REVENGEOS_BUILDTYPE := UNOFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build Description
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        TARGET_DEVICE="violet" \
+        PRODUCT_NAME="violet" \
+        PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
+	PRODUCT_NAME="violet"
+
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := revengeos_violet
+PRODUCT_DEVICE := violet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 7 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
