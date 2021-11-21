f35e68ed1a15222814f5cac41ad3db14afa55205
Raman <81064836+raman047@users.noreply.github.com>
2021-11-21 19:23:32 +0530

    Update revengeos_violet.mk

--- revengeos_violet.mk
+++ revengeos_violet.mk
@@ -1,37 +1,39 @@
+# Copyright (C) 2020-2021 Wave-OS
 #
-# Copyright (C) 2020 The LineageOS Project
-
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/revengeos/config/common.mk)
+# Inherit ANXCamera
+$(call inherit-product, vendor/ANXCamera/config.mk)
+
+# Inherit some common StagOS stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-REVENGEOS_BUILDTYPE := UNOFFICIAL
-TARGET_FACE_UNLOCK_SUPPORTED := true
+$(call inherit-product, vendor/stag/main.mk)
 
-# Build Description
-PRODUCT_BUILD_PROP_OVERRIDES += \
-        TARGET_DEVICE="violet" \
-        PRODUCT_NAME="violet" \
-        PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
-	PRODUCT_NAME="violet"
-
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
-
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := revengeos_violet
+PRODUCT_NAME := stag_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="violet-user 9 PKQ1.181203.001 V11.0.8.0.PFHINXM release-keys" \
+    PRODUCT_NAME="violet"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
